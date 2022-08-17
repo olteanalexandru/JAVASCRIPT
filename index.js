d5484728bdb076374b6c0dc0efc16ec619093138
Alexandru Oltean <oltean.alexandru11@gmail.com>
2022-08-17 21:16:36 +0300

    make it better

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 const connectDB = require('./config/db');
 const cors = require('cors');
+require('dotenv').config();
 connectDB();
 app.use(express.json());
 //middleware:
